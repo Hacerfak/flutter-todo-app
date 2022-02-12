6a5312fbc34afbd6efa0d8ed185c9a58a645aa5b
Hacerfak <hacerfak@outlook.com>
2022-02-12 10:30:03 -0300

    Refatoração

---
+++ lib/models/Home.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //Lista de tarefas, variáveis de uso comun
+  List _listaTarefas = [];
+  Map<String, dynamic> _ultimaTarefaRemovida = Map();
+  TextEditingController _controllerTarefa = TextEditingController();
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarTarefa() {
+    String textoDigitado = _controllerTarefa.text;
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = textoDigitado;
+    tarefa["realizada"] = false;
+
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+    _salvarArquivo();
+
+    _controllerTarefa.text = "";
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
+  _lerArquivo() async {
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
+  Widget criarItemLista(context, index) {
+    Color _getColor(BuildContext context) {
+      return _listaTarefas[index]['realizada'] //
+          ? Colors.black54
+          : Theme.of(context).primaryColor;
+    }
+
+    TextStyle? _getTextStyle(BuildContext context) {
+      if (!_listaTarefas[index]['realizada']) return null;
+
+      return const TextStyle(
+        color: Colors.black54,
+        decoration: TextDecoration.lineThrough,
+      );
+    }
+
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      //direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        //recuperar último item excluído
+        _ultimaTarefaRemovida = _listaTarefas[index];
+
+        //Remove item da lista
+        _listaTarefas.removeAt(index);
+        _salvarArquivo();
+
+        //snackbar
+        final snackbar = SnackBar(
+          //backgroundColor: Colors.green,
+          duration: Duration(seconds: 5),
+          content: Text("Tarefa removida!!"),
+          action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                //Insere novamente item removido na lista
+                setState(() {
+                  _listaTarefas.insert(index, _ultimaTarefaRemovida);
+                });
+                _salvarArquivo();
+              }),
+        );
+
+        Scaffold.of(context).showSnackBar(snackbar);
+      },
+      background: Container(
+        color: Colors.red,
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+      child: ListTile(
+        title: Text(
+          _listaTarefas[index]['titulo'],
+          style: _getTextStyle(context),
+        ),
+        leading: CircleAvatar(
+          backgroundColor: _getColor(context),
+          //child: Text(_listaTarefas[index]['titulo'][0]),
+          child: const Icon(Icons.star),
+        ),
+        onTap: () {
+          setState(() {
+            if (!_listaTarefas[index]['realizada']) {
+              _listaTarefas[index]['realizada'] = true;
+            } else {
+              _listaTarefas[index]['realizada'] = false;
+            }
+          });
+
+          _salvarArquivo();
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //_salvarArquivo();
+    //print("itens: " + DateTime.now().millisecondsSinceEpoch.toString() );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        //backgroundColor: Colors.yellow,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          //backgroundColor: Colors.yellow,
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Adicionar Tarefa"),
+                    content: TextField(
+                      controller: _controllerTarefa,
+                      decoration:
+                          InputDecoration(labelText: "Digite sua tarefa"),
+                      onChanged: (text) {},
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("Cancelar"),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      FlatButton(
+                        child: Text("Salvar"),
+                        onPressed: () {
+                          //salvar
+                          _salvarTarefa();
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          }),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                itemCount: _listaTarefas.length, itemBuilder: criarItemLista),
+          )
+        ],
+      ),
+    );
+  }
+}
