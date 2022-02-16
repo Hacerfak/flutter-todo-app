eefc286d9a72f451393f3f550a435d5696000f25
Hacerfak <hacerfak@outlook.com>
2022-02-16 20:30:38 -0300

    refatoração de arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo/models/Home.dart';
+import 'package:todo/models/home.dart';
 
 void main() => runApp(
       const MaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:todo/main.dart';
+import 'package:todo/models/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
